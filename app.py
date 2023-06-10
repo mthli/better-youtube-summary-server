cb84108a64012c74fb3b43c7ef54235e16ec5bd9
Matthew Lee <matthewlee0725@gmail.com>
2023-06-11 03:52:02 +0800

    feat: define errorhandler

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, abort, json, request
+from werkzeug.exceptions import HTTPException
+
+from constants import APPLICATION_JSON
+from logger import logger
+
+app = Flask(__name__)
+
+
+# https://flask.palletsprojects.com/en/2.2.x/errorhandling/#generic-exception-handler
+#
+# If no handler is registered,
+# HTTPException subclasses show a generic message about their code,
+# while other exceptions are converted to a generic "500 Internal Server Error".
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        'code': e.code,
+        'name': e.name,
+        'description': e.description,
+    })
+    response.content_type = APPLICATION_JSON
+    logger.error(f'errorhandler, data={response.data}')
+    return response
+
+
+# TODO
