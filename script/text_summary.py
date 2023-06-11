fea04057d2e9d276abd574a7c0041aa45a3918fc
Matthew Lee <matthewlee0725@gmail.com>
2023-06-11 13:54:29 +0800

    feat: parse chapters

--- script/text_summary.py
+++ script/text_summary.py
@@ -1,9 +1,89 @@
-from summary import _parse_timed_texts
+from summary import summarize
 
-VIDEO_ID = 'Ff4fRgnuFgQ'
+VID = 'Ff4fRgnuFgQ'
 FILE = 'text_summary_timedtext.xml'
+CHAPTERS = [
+    {
+        "title": "Introduction",
+        "timestamp": "0:00"
+    },
+    {
+        "title": "Jiu-jitsu competition",
+        "timestamp": "0:28"
+    },
+    {
+        "title": "AI and open source movement",
+        "timestamp": "17:51"
+    },
+    {
+        "title": "Next AI model release",
+        "timestamp": "30:22"
+    },
+    {
+        "title": "Future of AI at Meta",
+        "timestamp": "42:37"
+    },
+    {
+        "title": "Bots",
+        "timestamp": "1:03:15"
+    },
+    {
+        "title": "Censorship",
+        "timestamp": "1:18:42"
+    },
+    {
+        "title": "Meta's new social network",
+        "timestamp": "1:33:23"
+    },
+    {
+        "title": "Elon Musk",
+        "timestamp": "1:40:10"
+    },
+    {
+        "title": "Layoffs and firing",
+        "timestamp": "1:44:15"
+    },
+    {
+        "title": "Hiring",
+        "timestamp": "1:51:45"
+    },
+    {
+        "title": "Meta Quest 3",
+        "timestamp": "1:57:37"
+    },
+    {
+        "title": "Apple Vision Pro",
+        "timestamp": "2:04:34"
+    },
+    {
+        "title": "AI existential risk",
+        "timestamp": "2:10:50"
+    },
+    {
+        "title": "Power",
+        "timestamp": "2:17:13"
+    },
+    {
+        "title": "AGI timeline",
+        "timestamp": "2:20:44"
+    },
+    {
+        "title": "Murph challenge",
+        "timestamp": "2:28:07"
+    },
+    {
+        "title": "Embodied AGI",
+        "timestamp": "2:33:22"
+    },
+    {
+        "title": "Faith",
+        "timestamp": "2:36:29"
+    }
+]
 
-with open(FILE, 'r') as f:
-    file = f.read()
 
-timed_texts = _parse_timed_texts(VIDEO_ID, file)
+async def test_summary():
+    with open(FILE, 'r') as f:
+        file = f.read()
+
+    # TODO
--- summary.py
+++ summary.py
@@ -1,7 +1,24 @@
+import json
+import re
+
 from dataclasses import dataclass
 
 from bs4 import BeautifulSoup
 from flask import abort
+from logger import logger
+from openai import Role, TokenLimit, \
+    build_message, \
+    chat, \
+    count_tokens, \
+    get_content
+
+
+@dataclass
+class Chapter:
+    timestamp: str = 0  # required.
+    seconds: int = 0    # required.
+    chapter: str = ''   # required.
+    summary: str = ''   # optional.
 
 
 @dataclass
@@ -11,9 +28,13 @@ class TimedText:
     text: str = ''    # required.
 
 
-async def summarize(vid: str, timedtext: str, chapters: list[str] = [], lang: str = 'en'):
+async def summarize(vid: str, timedtext: str, chapters: list[dict] = [], lang: str = 'en'):
     timed_texts = _parse_timed_texts(vid, timedtext)
-    # TODO
+
+    chapters = _parse_chapters(vid, chapters)
+    if not chapters:
+        # TODO
+        pass
 
 
 def _parse_timed_texts(vid: str, src: str) -> list[TimedText]:
@@ -39,3 +60,33 @@ def _parse_timed_texts(vid: str, src: str) -> list[TimedText]:
         abort(400, f'transcript format invalid, vid={vid}')
 
     return timed_texts
+
+
+def _parse_chapters(vid: str, chapters: list[dict]) -> list[Chapter]:
+    res: list[Chapter] = []
+
+    if not chapters:
+        logger.info(f'parse chapters, but chapters is empty, vid={vid}')
+        return res
+
+    try:
+        for c in chapters:
+            timestamp: str = c['timestamp']
+
+            seconds: int = 0
+            array: list[str] = timestamp.split(':')
+            if len(array) == 2:
+                seconds = int(array[0]) * 60 + int(array[1])
+            elif len(array) == 3:
+                seconds = int(array[0]) * 60 * 60 + int(array[1]) * 60 + int(array[2])  # nopep8.
+
+            res.append(Chapter(
+                timestamp=timestamp,
+                seconds=seconds,
+                chapter=c['title'],
+            ))
+    except Exception:
+        logger.exception(f'parse chapters failed, vid={vid}')
+        return res
+
+    return res
